7b12090c4bc6981b1967600f4089a6e147198cfe
Adrian Rolloque <drnpaulrolloque@gmail.com>
2025-04-10 00:07:09 +0800

    [DEV.Learn.Tutorials.MVC] initial commit, base project

---
+++ DEV.Learn/DEV.Learn.Tutorials.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DEV.Learn.Tutorials.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
