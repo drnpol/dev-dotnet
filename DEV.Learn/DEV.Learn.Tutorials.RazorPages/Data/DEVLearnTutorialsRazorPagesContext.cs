292a66331a563630d3410f4139faa0111c27c856
Adrian Rolloque <drnpaulrolloque@gmail.com>
2025-04-10 00:05:24 +0800

    [Dev.Learn.Tutorials.RazorPages] Initial commit with Scaffolded Movie views

---
+++ DEV.Learn/DEV.Learn.Tutorials.RazorPages/Data/DEVLearnTutorialsRazorPagesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DEV.Learn.Tutorials.RazorPages.Models;
+
+namespace DEV.Learn.Tutorials.RazorPages.Data
+{
+    public class DEVLearnTutorialsRazorPagesContext : DbContext
+    {
+        public DEVLearnTutorialsRazorPagesContext (DbContextOptions<DEVLearnTutorialsRazorPagesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DEV.Learn.Tutorials.RazorPages.Models.Movie> Movie { get; set; } = default!;
+    }
+}
